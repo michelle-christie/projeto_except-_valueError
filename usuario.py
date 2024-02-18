65f26675c94dba907b82b399f645b64ff0723dfa
Michelle Christie <113917878+michelle-christie@users.noreply.github.com>
2024-02-17 23:12:41 -0400

    Create usuario.py
    Projeto Python except ValueError

---
+++ usuario.py
@@ -0,0 +1,12 @@
+nome = input('Digite seu nome completo: ').strip()
+while True:
+    try:
+        nascimento = int(input('Digite o ano de nascimento (entre 1922 e 2021): '))
+        if nascimento < 1922 or nascimento > 2021:
+            raise ValueError('Ano não permitido')
+        idade = 2022 - nascimento
+        print(f'Olá, {nome}! Você completou ou completará {idade} anos neste ano 2022.')
+        break
+    except ValueError:
+        print('Erro: inválido. Por favor, digite novamente o seu ano de nascimento.')
+
