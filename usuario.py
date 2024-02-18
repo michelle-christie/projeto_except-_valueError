09c41fbd6b88c97d87c86884c22460f75b6c1fe4
Michelle Christie <113917878+michelle-christie@users.noreply.github.com>
2024-02-17 23:19:32 -0400

    Update usuario.py
    Eliminei a palavra completará do código, já que o ano 2022 é passado.

--- usuario.py
+++ usuario.py
@@ -5,7 +5,7 @@ while True:
         if nascimento < 1922 or nascimento > 2021:
             raise ValueError('Ano não permitido')
         idade = 2022 - nascimento
-        print(f'Olá, {nome}! Você completou ou completará {idade} anos neste ano 2022.')
+        print(f'Olá, {nome}! Você completou {idade} anos no ano 2022.')
         break
     except ValueError:
         print('Erro: inválido. Por favor, digite novamente o seu ano de nascimento.')
